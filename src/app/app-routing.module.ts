33f11e73cbe8de91ee3cafe111cc2551d65eed55
Mahmouud74 <64709199+Mahmouud74@users.noreply.github.com>
2022-02-15 11:25:32 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { EmpComponent } from './emp/emp.component';
 import { FormitselfComponent } from './formitself/formitself.component';
 import { MgrComponent } from './mgr/mgr.component';
-const routes:Routes = [{path:"demo",component:EmpComponent},
-{path:"mgr",component:MgrComponent}];
+const routes:Routes = [
+  {path:"demo",component:EmpComponent},
+  {path:"mgr",component:MgrComponent},
+  {path:"" , redirectTo:"/demo",pathMatch:"full"}
+  ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
